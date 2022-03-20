4dc11e825dc26bad0b6505fa0a53f70f8d161864
matteo83 <makkiato@protonmail.com>
2022-03-20 17:22:55 +0100

    update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # tp  - Tor Pipe
 
-A minimalistic script (just one .py file) to *pipe* over tor. It uses the Python standard library and the the `stem` package. 
+A small script (just one .py file) to _pipe_ over tor. It uses the Python standard library and the `stem` package. 
 
 ## Installation
 ```
@@ -11,32 +11,79 @@ pip install --editable .
 
 ## Description
 
-`tp` connects to the Tor SOCKS proxy (use the `--port` modifier to provide the port, default is 9150 which is used by the Tor Browser), forks into a Hidden HTTP server on the fly and terminates.
+`tp` gets as input the `stdin` and does the following:
+1) connects to the Tor SOCKS proxy (use the `--port` modifier to provide the port, default is 9151 which is used by the Tor Browser), 
+2) forks into a Hidden HTTP server on the fly and terminates. 
+    - using option (`-b`/`--background`) `tp` does not fork and terminates together with the HTTP server. This could be useful to know when/if somebody got the content.
 
-The spawned hidden HTTP service runs in the background until it receives a `GET` requests. It then delivers the serves the `stdin` and terminates.
-
-
-This means that the data is delivered to most to one recipient.
+The spawned hidden HTTP service runs in the background until it receives a `GET` requests. It then delivers the input (`stdin`) and terminates.
+This means that the data is delivered to at most one recipient.
 
 **Option -b/--blocking**: alternatively, with the flag `--blocking` or `-b`, `tp` returns only when the content is delivered.
 
 ### Example 1:
-Machine1: \
-`cat README.md | tp`\
-` sdout> ndefqjsjly6h7a4ysra25jhubo7eipqfyb43fmvjxkuxixp4vnxixeyd.onion`
+Assume Alice and Bob are in separate networks.\
+Alice can share a document with Bob as follows:
+```
+alice:~ cat README.md | tp`
+ndefqjsjly6h7a4ysra25jhubo7eipqfyb43fmvjxkuxixp4vnxixeyd.onion
+```
 
-Machine2: \
-`torsocks curl ndefqjsjly6h7a4ysra25jhubo7eipqfyb43fmvjxkuxixp4vnxixeyd.onion >> received.txt`
+Then Alice communicates by some mean the onion address to Bob.\
+Bob can then receive as follows:
 
+```
+bob:~ torsocks curl ndefqjsjly6h7a4ysra25jhubo7eipqfyb43fmvjxkuxixp4vnxixeyd.onion >> received.txt
+```
+The communication is fully encrypted by the TOR onion protocol.\
+Note, however, that Alice does not know if it was Bob or somebody else to receive the file!
 
 ### Example 2:
 
-Machine1: \
-`echo "Plese write to me at myemail@example.com" | tp`\
-`sdout > mpdiziullb63u3577kpnucm5lojaivfifkaxxvzqrqot3pgfdb3ijoqd.onion`\
-Machine 2:\
-`torsocks curl mpdiziullb63u3577kpnucm5lojaivfifkaxxvzqrqot3pgfdb3ijoqd.onion`\
-`sdout >  Plese write to me at myemail@example.com`
+Alice wants to share her mail with Bob in a private way. She also wants to see when the message is delivered.
+
+```commandline
+alice:~ echo "Hi bob, write to me at myemail@example.com" | tp -b
+mpdiziullb63u3577kpnucm5lojaivfifkaxxvzqrqot3pgfdb3ijoqd.onion
+```
+
+```commandline
+bob:~ torsocks curl mpdiziullb63u3577kpnucm5lojaivfifkaxxvzqrqot3pgfdb3ijoqd.onion
+Plese write to me at myemail@example.com
+```
 
 ### Example 3:
+
+In Alice's machine, TOR is listening on port 7000.\
+Alice wants to send a gpg signed message to Bob in a private way.
+
+```commandline
+alice:~ gpg --clearsign | tp
+5uvzimj5263mnua5glux5qryzpgndidoz6shvrzwxn22gmz4qhzzlnqd.onion
+```
+
+Bob opens the onion address with the Tor browser and sees:
+
+```commandline
+-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA256
+
+Hi bob! This is a message from me, Alice :)
+-----BEGIN PGP SIGNATURE-----
+
+iQIzBAEBCAAdFiEEkQgDD8OI7C+4e0Mz3pfEO2irY0oFAmI3VKsACgkQ3pfEO2ir
+Y0oItw//f6xvdvirzpfLett0s9wC3bm7mZGt0W3IyKzIQoLd1Fr/oJjJxfoCiUVG
+EKsgsQuS1kVlvnjlkdK392UBsnBqT46kYHRqtu+yqE44Z7kdf+U0Jgr6IIxw0A6b
+pApIpaba4uL/GNhCxYYs9ZxpMAOa20NhN5b8OoVicpbJjqiZJ+/6yfJ3h1ZP+Bcd
+VD08mYM8aB5G1aN1m/FNSLdHd4lQARo/E2ie24yI3aj+EO2fLOEjyufXrhS21PPC
+Riu7XcGuuC6oSUsiP2QJouo9lFV44Y1iICx3+vVsH83PjbP+IEs3OyPllGYBlce+
+85Bimnqvxqyjfg1V1jS0tSsyJ5UjI9N5cNlBgyXZS1OOLPLfFKWzAVPHVMFIaHjl
+r7XXKrlQ1g4g9Drbli932/c8CuNcu+g8CRHa3gzAJ8+Y4qSIXJTBDUP1RofHomRp
+Lp563Ap/Ctn4OBR5kBkzcbZ3fKDpZsAF2NMIJEmugAQpR4oVT2pvc6716R5mgYrf
+smbHKaAldgU7fRMln1nn/JW/7/aNg61HOSHcBP9XtQnGc62y9bEGo+m1Yj28XrVM
+UuIJXDO6XwEobUzL0zto/aPm/HO+T6otxbHi574pub/c+U8iSfistfBUriiEnhuK
+LjWf1ugsp27cxJ7H2rj1sk14DSxAQE+pEPFFrpdmTktOSzzPXzI=
+=ylu9
+-----END PGP SIGNATURE-----
+```
 
--- tp.py
+++ tp.py
@@ -7,7 +7,6 @@ def catch_the_signal(signal, frame):
 signal.signal(signal.SIGINT, catch_the_signal)
 
 import os
-import logging
 import socket
 from threading import Thread
 from contextlib import closing
@@ -17,6 +16,14 @@ from stem import SocketError as StemSocketError
 from stem import process
 import click
 
+import logging
+import logging.config
+logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': True,
+})
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)
 
 
 # Handler for Http requests.
@@ -24,39 +31,40 @@ import click
 class MyHandler(BaseHTTPRequestHandler):
     data_to_send = None
     def do_GET(self):
-        is_unicode = True
-        try:
-            self.data_to_send.decode('utf-8')
-        except UnicodeDecodeError:
-            is_unicode = False
+        self.wfile.write(self.data_to_send)
 
-        self.send_response(200)
-        if is_unicode:
-            self.send_header("Content-type", "text/html; charset=utf-8")
-            self.end_headers()
-            self.wfile.write(self.data_to_send)
-        else:
-            self.send_header("Content-type", "application/octet-stream")
-            self.end_headers()
-            self.wfile.write(self.data_to_send)
+        # is_unicode = True
+        # unicode_text = ''
+        # try:
+        #     unicode_text = self.data_to_send.decode('ascii')
+        # except UnicodeDecodeError:
+        #     is_unicode = False
+        #
+        # self.send_response(200)
+        # if is_unicode:
+        #     self.send_header("Content-type", "text/html; charset=utf-8")
+        #     self.end_headers()
+        #     self.wfile.write(self.data_to_send)
+        # else:
+        #     self.send_header("Content-type", "application/octet-stream")
+        #     self.end_headers()
+        #     self.wfile.write(self.data_to_send)
 
 # Minimal HTTP server
 class Httpd():
-    def __init__(self, data,  logger, port = 5050):
+    def __init__(self, data, port = 5050):
         server_address = ('localhost', port)
         MyHandler.data_to_send = data
-        self.logger = logger
         self.server = HTTPServer(server_address, MyHandler)
         self.server.handle_request()
 
 
 class MainThread(Thread):
-    def __init__(self, data_to_send, logger, wait, port):
+    def __init__(self, data_to_send, wait, port):
         Thread.__init__(self)
         self.data_to_send = data_to_send
         self.controller = None
         self.service = None
-        self.logger = logger
         self.wait = wait
         self.port = port
 
@@ -84,7 +92,7 @@ class MainThread(Thread):
         # Print out Onion address.
         print(self.service.service_id + f'.onion')
 
-        self.logger.info(f'Successfully created Hidden service: {self.service.service_id + f".onion"}, listening on localhost:{local_port}.')
+        logger.info(f'Successfully created Hidden service: {self.service.service_id + f".onion"}, listening on localhost:{local_port}.')
 
         if not self.wait:
             n = os.fork()
@@ -93,37 +101,23 @@ class MainThread(Thread):
                 sys.exit()
 
         # Start HTTP server on localhost:local_port
-        server = Httpd(self.data_to_send, port=local_port, logger = self.logger)
+        server = Httpd(self.data_to_send, port=local_port)
 
         # Once the page has been served, remove the hidden service and terminate
         self.controller.remove_ephemeral_hidden_service(self.service.service_id)
-        self.logger.info(f'Hidden service {self.service.service_id}.onion properly terminated.')
+        logger.info(f'Hidden service {self.service.service_id}.onion properly terminated.')
         sys.exit()
 
     def handle_keyboardInterrupt(self, signal, frame):
         try:
             self.controller.remove_ephemeral_hidden_service(self.service.service_id)
-            self.logger.info(f'Hidden service {self.service.service_id}.onion properly terminated.')
-            self.logger.info('Exiting (CTRL-C).')
+            logger.info(f'Hidden service {self.service.service_id}.onion properly terminated.')
+            logger.info('Exiting (CTRL-C).')
             sys.exit()
         except Exception as e:
-            self.logger.error(f'Error while exiting (CTRL-C): {e}')
+            logger.error(f'Error while exiting (CTRL-C): {e}')
             sys.exit()
 
-
-# create logger
-logger = logging.getLogger("TP")
-logger.setLevel(logging.ERROR)
-# create console handler and set level to debug
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-# create formatter
-# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-formatter = logging.Formatter('%(levelname)s : %(message)s')
-# add formatter to ch
-ch.setFormatter(formatter)
-# add ch to logger
-logger.addHandler(ch)
 
 
 @click.command()
@@ -133,9 +127,23 @@ def cli(blocking, port):
     """
     Documentation
     """
+
+    # create logger
+    logger.setLevel(logging.ERROR)
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.ERROR)
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    # formatter = logging.Formatter('%(levelname)s : %(message)s')
+    # add formatter to ch
+    ch.setFormatter(formatter)
+    # add ch to logger
+    logger.addHandler(ch)
+
     try:
         data_input = sys.stdin.buffer.read()
-        thread = MainThread(data_to_send=data_input, logger=logger, wait = blocking, port = port)
+        thread = MainThread(data_to_send=data_input, wait = blocking, port = port)
         thread.run()
     except StemSocketError:
         logger.critical('Could not connect to the Tor socks. Is Tor Running?')
